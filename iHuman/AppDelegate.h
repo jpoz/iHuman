5edd2c4ecd8dcdb3f2594ce6005575f1e1502034
James Pozdena <jpoz@jpoz.net>
2011-10-26 20:57:35 -0700

    Initial Commit

---
+++ iHuman/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  iHuman
+//
+//  Created by James Pozdena on 10/26/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ iHuman/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iHuman
+//
+//  Created by James Pozdena on 10/26/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
